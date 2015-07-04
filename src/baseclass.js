46ccbad4723b9f6e0e05acdd3cf70960eddf9773
Dan <cobbdb@gmail.com>
2015-07-04 11:17:50 -0700

    Provide _create method for introspection after creation

--- src/baseclass.js
+++ src/baseclass.js
@@ -1,17 +1,32 @@
-var rebind = require('./rebind.js');
+var rebind = require('./rebind.js'),
+    Stub = require('./stub.js');
 
+/**
+ * @class BaseClass
+ * @param {Object} [root] The most senior parent
+ * of the inheritance chain.
+ */
 function contructor(root) {
+    root = root || {};
+
+    /**
+     * Provide hook for subclasses.
+     * @param {Object} [child] The next child.
+     */
     root.extend = function (child) {
         var key, base = {
             base: root.base
         };
         child = child || {};
 
+        // Rebind methods to use correct child.
         for (key in root) {
             if (typeof root[key] === 'function') {
                 base[key] = root[key].bind(root);
             }
         }
+
+        // Inherit from parent.
         for (key in child) {
             if (typeof child[key] === 'function') {
                 root[key] = rebind(key, root, base, child);
@@ -20,10 +35,20 @@ function contructor(root) {
             }
         }
 
+        // Provide back link.
         root.base = base;
+
+        // Execute any construction logic.
+        root._create.call(root);
+
         return root;
     };
 
+    /**
+     * Provide hook for interfaces.
+     * @param {Interface} Any number of Interfaces
+     * to apply.
+     */
     root.implement = function () {
         var i, len = arguments.length;
         for (i = 0; i < len; i += 1) {
@@ -32,11 +57,20 @@ function contructor(root) {
         return root;
     };
 
+    /**
+     * Run any construction logic.
+     */
+    if ('_create' in root) {
+        root._create.call(root);
+    } else {
+        root._create = Stub;
+    }
+
     return root;
 }
 
 contructor.Abstract = require('./abstract.js');
-contructor.Stub = require('./stub.js');
+contructor.Stub = Stub;
 contructor.Interface = require('./interface.js');
 
 module.exports = contructor;
---
+++ tests/_create.spec.js
@@ -0,0 +1,42 @@
+﻿var BaseClass = require('../src/baseclass.js');
+
+describe('Creation logic', function () {
+    it('is optional', function () {
+        expect(function () {
+            var Test = BaseClass();
+        }).not.toThrow();
+    });
+    it('runs only once per inheritance level', function () {
+        var count = 0;
+        var TestParent = BaseClass({
+            _create: function () {
+                count += 1;
+            }
+        });
+        expect(count).toEqual(1);
+        var TestChild = TestParent.extend();
+        expect(count).toEqual(2);
+    });
+    it('can be overridden', function () {
+        var count = 0;
+        var TestParent = BaseClass({
+            _create: function () {
+                count += 1;
+            }
+        });
+        expect(count).toEqual(1);
+        var TestChild = TestParent.extend({
+            _create: function () {}
+        });
+        expect(count).toEqual(1);
+    });
+    it('is bound to its own instance', function (done) {
+        var TestParent = BaseClass({
+            name: 'test-name',
+            _create: function () {
+                expect(this.name).toEqual('test-name');
+                done();
+            }
+        });
+    });
+});
